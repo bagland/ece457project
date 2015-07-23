9c9d93345d4facc29e9b9319a8bb27c86ec2f1ae
unknown <ornalth@hotmail.com>
2015-07-23 15:16:12 -0400

    Add evaluate soln, more SA changes

--- SA/SA.m
+++ SA/SA.m
@@ -1,15 +1,32 @@
+%User Input requirements, starting location + what they want to purchase
+currentPurchaseArray = {'Apples', 'Chicken', 'Oranges', 'Duck', 'VeryExpensiveItem'};
+startLocation = 'Location_1';
 
-purchaseArray = {'Apples', 'Chicken'};
-startLocation = 'ZLocation_1';
+%Standard SA params.
+boltzman = 1;
+initialTemp =1.0; 
+maxNumRuns = 1000;
+alpha=0.95; % Cooling factor
+temperature = initialTemp;
+runNum = 0;
 
+swapProbability = 0.5;
+randomStoreProbability = 1 - swapProbability;
+
+%Objective Fcn
+weightDist = 0.5;
+weightPrice = 1 - weightDist;
 
 distanceMap = parse_distances('outputDistance.txt');
 inventoryMap = parse_inventory('outputInventory.txt');
+storeNames = store_names('outputDistance.txt');
+numItems = size(currentPurchaseArray);
 
-storeList = cell(size(purchaseArray));
-
+%Generating an initial soln------------
+storeList = cell(numItems);
 count = 0;
-for itemName = purchaseArray
+%Get Stores that sell the items we want.
+for itemName = currentPurchaseArray
     itemCharName = itemName{1};
     storeItemMap = inventoryMap(itemCharName);
     storeKeys = keys(storeItemMap);
@@ -17,8 +34,10 @@ for itemName = purchaseArray
     storeList{count} = storeKeys{1};
 end
 
+%Select 1st store that sells each item as initial soln and generate route
+%Route is initialLoc, store1, store2...., storeN, initialLoc
 midRoute{1} = startLocation;
-currentStoreList = cell(size(purchaseArray));
+currentStoreList = cell(size(currentPurchaseArray));
 count = 2;
 for loc = storeList
     midRoute{count} = loc{1};
@@ -27,62 +46,99 @@ for loc = storeList
 end
 midRoute{count} = startLocation;
 
-evalSolnCost = evaluateSoln(midRoute,purchaseArray,currentStoreList)
-
-% Initializing parameters and settings
-T_init =1.0; % Initial temperature
-T_min = 1e-10; % Final stopping temperature
-%F_min = -1e+100; % Min value of the function
-%max_rej=2500; % Maximum number of rejections
-max_run=500; % Maximum number of runs
-%max_accept = 15; % Maximum number of accept
-k = 1; % Boltzmann constant
-alpha=0.95; % Cooling factor
-%Enorm=1e-8; % Energy norm (eg, Enorm=le-8)
-%guess=[2 2]; % Initial guess
-
-% Initializing the counters i,j etc
-%i= 0; j = 0; accept = 0; totaleval = 0;
-
-% Initializing various values
-temperature = T_init;
-%E_init = f(guess(1),guess(2));
-%E_old = E_init; E_new=E_old;
-%best=guess; % initially guessed values
-runNum = 0;
-
-bestEval = evalSolnCost;
+%get initial soln cost
+[distCost, priceCost] = evaluateSoln(midRoute,currentPurchaseArray,currentStoreList, distanceMap, inventoryMap, storeNames);
+currentSolnCost = weightDist * distCost + weightPrice * priceCost;
 
 
-while (runNum < 1)
+%Stored best soln for output
+bestSolnCost = currentSolnCost;
+bestcurrentPurchaseArray = currentPurchaseArray;
+bestStoreList = currentStoreList;
+
+%Stored current soln to compare against (we may have accepted a worse soln
+%than the best)
+iterSolnCost = currentSolnCost;
+itercurrentPurchaseArray = currentPurchaseArray;
+iterStoreList = currentStoreList;
+
+while (runNum < maxNumRuns)
     
     %swap
-    
-    %Swap the items in the grocery list
-    temp = purchaseArray{1};
-    purchaseArray{1} = purchaseArray{2};
-    purchaseArray{2} = temp;
-    
-    %Swap them in the reference store list. (need these keys in order)
-    temp = storeList{1};
-    storeList{1} = storeList{2};
-    storeList{2} = temp;
-    
-    %Swap in the route. note the +1, zzz.
-    temp = midRoute{2};
-    midRoute{2} = midRoute{3};
-    midRoute{3} = temp;
-    
-    %swap the current store order.
-    temp = currentStoreList{1};
-    currentStoreList{1} = currentStoreList{2};
-    currentStoreList{2} = temp;
-    
-    evalSolnCost = evaluateSoln(midRoute,purchaseArray,currentStoreList)
-    %random store.
-    
-    
+    if (rand() < swapProbability)
+        %disp('swap');
+        firstSlot = randi(numItems);
+        secondSlot = randi(numItems);
+        
+        % assume > 1 items. or else infinite loop
+        while secondSlot == firstSlot
+           secondSlot = randi(numItems);
+        end
+        
+        %Swap the items in the grocery list
+        temp = currentPurchaseArray{firstSlot};
+        currentPurchaseArray{firstSlot} = currentPurchaseArray{secondSlot};
+        currentPurchaseArray{secondSlot} = temp;
+
+        %Swap them in the reference store list. (need these keys in order)
+        temp = storeList{firstSlot};
+        storeList{firstSlot} = storeList{secondSlot};
+        storeList{secondSlot} = temp;
+
+        %Swap in the route. note the +1, zzz.
+        temp = midRoute{firstSlot+1};
+        midRoute{firstSlot+1} = midRoute{secondSlot+1};
+        midRoute{secondSlot+1} = temp;
+
+        %swap the current store order.
+        temp = currentStoreList{firstSlot};
+        currentStoreList{firstSlot} = currentStoreList{secondSlot};
+        currentStoreList{secondSlot} = temp;
+    else
+        %Random Store
+        %disp('randomstore.');
+        whatItem = randi(numItems); %Pick which item select a different store.
+        itemCharName = currentPurchaseArray{whatItem};
+        storeItemMap = inventoryMap(itemCharName); %Get what stores sell that item
+        storeKeys = keys(storeItemMap);
+        whatStore = randi(size(storeKeys)); % Pick a random store in that list
+        currentStoreList{whatItem} = storeKeys{whatStore};
+    end
+   
+    %Eval soln
+    [distCost, priceCost] = evaluateSoln(midRoute,currentPurchaseArray,currentStoreList, distanceMap, inventoryMap, storeNames);
+    currentSolnCost = weightDist * distCost + weightPrice * priceCost;
+
+    %Best soln we've seen so far
+    if (currentSolnCost < bestSolnCost)
+        bestSolnCost = currentSolnCost;
+        bestcurrentPurchaseArray = currentPurchaseArray;
+        bestStoreList = currentStoreList;
+        iterSolnCost = currentSolnCost;
+        itercurrentPurchaseArray = currentPurchaseArray;
+        iterStoreList = currentStoreList;
+    %Better than the current soln we have
+    elseif (currentSolnCost < iterSolnCost)
+        iterSolnCost = currentSolnCost;
+        itercurrentPurchaseArray = currentPurchaseArray;
+        iterStoreList = currentStoreList;
+    else
+        %SA check probability of acceptance.
+        difference = currentSolnCost - iterSolnCost;
+        if exp(-difference/(boltzman*temperature))>rand()
+           %Accept when worse 
+           iterSolnCost = currentSolnCost;
+           itercurrentPurchaseArray = currentPurchaseArray;
+           iterStoreList = currentStoreList;
+        end
+            
+    end
     %cooldown
+    %Can change this too.
     temperature = temperature * alpha;
     runNum = runNum + 1;
 end
+disp('Best soln');
+disp(bestcurrentPurchaseArray);
+disp (bestStoreList);
+disp(bestSolnCost);
