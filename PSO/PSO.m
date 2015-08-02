044b3b730f6a0b2c204270c0fd105bd2b964ecab
Ahson Khan <aa26khan@uwaterloo.ca>
2015-08-02 18:58:58 -0400

    removed extra commented code and tweaked some parameters

--- PSO/PSO.m
+++ PSO/PSO.m
@@ -21,18 +21,18 @@ numItems = size(currentPurchaseArray);
 %Initial paramters
 numParticles = 10;  %size of the swarm
 probDim = 2;    %unused, dimension of the problem
-maxIterations = 10000;   %maximum number of iterations
+maxIterations = 5000;   %maximum number of iterations
 swarmSize = 10; %unused
 neighbourhoodSize = 1;  %unused (whole swarm is a neighbourhood)
 c1 = 1.1; %acceleration coefficient - cognitive parameter
 c2 = 4-c1;  %acceleration coefficient - social parameter
-w = 0.6;  %inertia weight
+w = 0.6;  %inertia weight, used for adaptation
 
 %Other parameters
 noIterImprovement = 0;
-noIterImprovementExit = 1000;
+noIterImprovementExit = maxIterations/4;
 
-%Adaptation paramter
+%Adaptation paramters
 adaptCounter = 0;
 noIterImprovementAdapt = noIterImprovementExit/10;
 
@@ -69,67 +69,6 @@ for itemName = currentPurchaseArray
     end
     count = count + 1;
 end
-
-possibleStores
-% stores = [14 1 0 12 11 16 1];
-% originalItemsList = [1 2 3 4 5 6 7];
-% items = [2 3 5 7 6 1 4];
-% getNew = [1 0 0 0 0 1 0];
-% 
-% possibleStores = cell(7, 6);
-% possibleStores
-% 
-% possibleStores{1,1} = 11;
-% possibleStores{1,2} = 16;
-% possibleStores{1,3} = 19;
-% possibleStores{1,4} = 27;
-% possibleStores{1,5} = 3;
-% possibleStores{1,6} = 4;
-% 
-% possibleStores{2,1} = 12;
-% possibleStores{2,2} = 14;
-% possibleStores{2,3} = 17;
-% possibleStores{2,4} = 2;
-% possibleStores{2,5} = 22;
-% possibleStores{2,6} = 6;
-% 
-% possibleStores{3,1} = 1;
-% possibleStores{3,2} = 13;
-% possibleStores{3,3} = 22;
-% possibleStores{3,4} = 23;
-% possibleStores{3,5} = 3;
-% possibleStores{3,6} = 5;
-% 
-% possibleStores{4,1} = 1;
-% possibleStores{4,2} = 11;
-% possibleStores{4,3} = 15;
-% possibleStores{4,4} = 18;
-% possibleStores{4,5} = 2;
-% possibleStores{4,6} = 5;
-% 
-% possibleStores{5,1} = 0;
-% possibleStores{5,2} = 29;
-% possibleStores{5,3} = 4;
-% possibleStores{5,4} = 8;
-% possibleStores{5,5} = 9;
-% possibleStores{5,6} = 7;
-% 
-% possibleStores{6,1} = 12;
-% possibleStores{6,2} = 16;
-% possibleStores{6,3} = 2;
-% possibleStores{6,4} = 27;
-% possibleStores{6,5} = 4;
-% possibleStores{6,6} = 5;
-% 
-% possibleStores{7,1} = 18;
-% possibleStores{7,2} = 2;
-% possibleStores{7,3} = 24;
-% possibleStores{7,4} = 26;
-% possibleStores{7,5} = 27;
-% possibleStores{7,6} = 9;
-% possibleStores
-% possibleStores = [11 16 19 27 3 4; 12 14 17 2 22 6; 1 13 22 23 3 5; 1 11 15 18 2 5; 0 29 4 8 9 7; 12 16 2 27 4 5; 18 2 24 26 27 9];
-% newStores = GetDiffStore(stores, items, originalItemsList, getNew, possibleStores);
 
 for i = 1:numParticles
     storeList = cell(numItems);
@@ -185,9 +124,7 @@ thePlot = plot(solnXAxis, solnYAxis, 'YDataSource', 'solnYAxis', 'XDataSource', 
 iter = 0;
 totalLoopTimeTaken = 0;
 while (iter < maxIterations && noIterImprovement < noIterImprovementExit)
-    
     tic
-    
     for i = 1:numParticles
        %Get random values between 0 and 1
        r1 = rand;
@@ -195,101 +132,30 @@ while (iter < maxIterations && noIterImprovement < noIterImprovementExit)
        inertia = velStores(i,:)*w;
        cognitive = FindDiff(pbestPurchaseArray(i,:), currPurchaseArray(i,:),  pbestStoreList(i, :), currStoreList(i,:))*r1*c1;
        social = FindDiff(bestcurrentPurchaseArray, currPurchaseArray(i,:), bestStoreList, currStoreList(i,:))*r1*c1;
-%        if (i==1)
-%             inertia
-%             cognitive
-%             social
-%             pbestSol = pbestStoreList(i, :)
-%             pbestList = pbestPurchaseArray(i,:)
-%             gbestSol = bestStoreList
-%             gbestList = bestcurrentPurchaseArray
-%        end       
+    
        %v[t+1] = w*v[t] + c1*rand()*(pbest[]-x[t]) + c2*rand()*(gbest[]-x[t])
        temp = inertia*probabilityDistribution(1) + cognitive*probabilityDistribution(2) + social*probabilityDistribution(3);
-%         if (i == 1)
-%             inertia
-%             cognitive
-%             social
-%             temp
-%             myStore =  currStoreList(i,:)
-%             myList = currPurchaseArray(i,:)
-%             pbestSol = pbestStoreList(i, :)
-%              pbestList = pbestPurchaseArray(i,:)
-%              gbestSol = bestStoreList
-%              gbestList = bestcurrentPurchaseArray
-%         end
+
        [a, b] = size(temp);
        for j = 1:b
            velStores(i,j) = temp(j);
        end
        %x[t+1] = x[t] + v[t+1]
-       %currPurchaseArray(i,:) = GetDiffStore(currPurchaseArray(i,:),temp); % Doesn't change
-       
-%        if (i == 1)
-%        before = currStoreList(i,:);
-%        addingVal = temp;
-%        end
        currStoreList(i,:) = GetDiffStore(currStoreList(i,:), currPurchaseArray(i,:), originalItemsList, temp, possibleStores);
-        
-%        if (i==1)
-%            after = currStoreList(i,:);
-%            itemsList = currPurchaseArray(i,:)
-%            before
-%             after
-%             addingVal
-%        end
 
        %Get random values between 0 and 1
        r1 = rand;
        r2 = rand;
        
        particleVelocity = velocity(:,:,i);
+       %Reset the old velocity vector
        velocity(:,:,i) = 0;
-%        if (i==1)
-%            velocity(:,:,i)
-%        end
-%        [dim1, dim2] = size(velocity(:,:,i));
-%        if (a ~= 0)
-%            for j = 1:a
-%                for k = 1:b
-%                    velocity(j,k,i) = 0;
-%                end
-%            end
-%        end
-%        velocity(:,:,i)
        particleVelocity(any(particleVelocity==0,2),:) = [];
-%          if (i == 1)
-%              check1 = particleVelocity
-%              check2 = velocity(:,:,i)
-%          end
     
        inertia = Multiply(particleVelocity, w);
-%        if (i == 1)
-%        particleVelocity
-%        w
-%        inertia
-%        end
-% if (i == 1)
-%     pbestPurchaseArray(i,:)
-%     currPurchaseArray(i,:)
-%     size(Subtracting(bestcurrentPurchaseArray, currPurchaseArray(i,:)))
-%     Subtracting(bestcurrentPurchaseArray, currPurchaseArray(i,:))
-% end
-
        cognitive = Multiply(Subtracting(pbestPurchaseArray(i, :), currPurchaseArray(i,:)), r1*c1);
        social = Multiply(Subtracting(bestcurrentPurchaseArray, currPurchaseArray(i,:)), r2*c2);
        %v[t+1] = w*v[t] + c1*rand()*(pbest[]-x[t]) + c2*rand()*(gbest[]-x[t])
-       
-%        if( i == 1)
-%        inertia
-%        cognitive
-%        social
-%        end
-       
-% TODO - fix the augmentation/truncation to use value correctly
-% TODO - increase particle size/etc or tweak parameters to get convergence
-% TODO - test if one or both are converging
-
        temp = [inertia;cognitive;social];
        [a, b] = size(temp);
        if (a ~= 0)
@@ -299,22 +165,10 @@ while (iter < maxIterations && noIterImprovement < noIterImprovementExit)
                end
            end
        end
-%         if (i == 1)
-%             next = velocity(:,:,i)
-%                end
-       
-%        if (i==1)
-%        temp
-%        beforeList = currPurchaseArray(i,:)
-%        beforeStore = currStoreList(i,:)
-%        end
        %x[t+1] = x[t] + v[t+1]
        currPurchaseArray(i,:) = Adding(currPurchaseArray(i,:), temp);
        currStoreList(i,:) = Adding(currStoreList(i,:), temp);
-%        if (i==1)
-%        afterList = currPurchaseArray(i,:)
-%        afterStore = currStoreList(i,:)
-%        end
+
     end
 
     for i = 1:numParticles
